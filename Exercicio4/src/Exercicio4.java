e21ebe01f040d432c40e31b24627d953d86c385c
newzko91 <tavares.gabri@gmail.com>
2017-02-20 21:40:39 -0300

    Exercicio4
    construa as tabuadas do 1 ao 5 imprimindo-as

---
+++ Exercicio4/src/Exercicio4.java
@@ -0,0 +1,53 @@
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		
+		int cont = 1;
+		
+		for(int i=0;i<=10;i++) {
+			int result;
+			result = cont*i;
+			System.out.println(cont + " x " + i + " = " + result);	
+		} 
+		
+		cont +=1;
+		System.out.println("\n");
+		
+		for(int i=0;i<=10;i++) {
+			int result;
+			result = cont*i;
+			System.out.println(cont + " x " + i + " = " + result);	
+		} 
+		
+		cont +=1;
+		System.out.println("\n");
+		
+		for(int i=0;i<=10;i++) {
+			int result;
+			result = cont*i;
+			System.out.println(cont + " x " + i + " = " + result);	
+		} 
+		
+		cont +=1;
+		System.out.println("\n");
+		
+		for(int i=0;i<=10;i++) {
+			int result;
+			result = cont*i;
+			System.out.println(cont + " x " + i + " = " + result);	
+		} 
+		
+		cont +=1;
+		System.out.println("\n");
+		
+		for(int i=0;i<=10;i++) {
+			int result;
+			result = cont*i;
+			System.out.println(cont + " x " + i + " = " + result);	
+		} 
+		
+
+	}
+
+}
