f9652a260003586487b94c41c5658eb1c1046931
newzko91 <tavares.gabri@gmail.com>
2017-02-20 21:04:59 -0300

    Add files via upload

---
+++ Exercicio1/src/Exercicio1.java
@@ -0,0 +1,13 @@
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=100;i++) {
+			if(i%19==0) {				
+				System.out.println("Achei um número divisivel por 19:" + " " + i);
+				break;
+			} 
+		}
+	}
+}
---
+++ Exercicio2/src/Exercicio2.java
@@ -0,0 +1,14 @@
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<100;i++) {
+		if(i>50 && i<60) {
+			System.out.println(i);
+			continue;
+		}
+	}
+
+	}
+
+}
