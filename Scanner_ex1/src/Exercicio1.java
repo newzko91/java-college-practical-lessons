958bc080bb5e868b462a198787a61924507043c1
newzko91 <tavares.gabri@gmail.com>
2017-04-24 20:29:53 -0300

    Scanner
    Scanner

---
+++ Scanner_ex1/src/Exercicio1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		String nome;
+		int idade;
+		double salario;
+
+		Scanner objLeitura = new Scanner(System.in);
+
+		System.out.print("Digite o nome completo: ");
+		nome = objLeitura.nextLine();
+		System.out.print("Digite a idade: ");
+		idade = objLeitura.nextInt();
+		System.out.print("Digite o valor do salario desejado: ");
+		salario = objLeitura.nextDouble();
+
+		System.out.println("\nNome: " + nome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Salario: " + salario);
+
+	}
+
+}
