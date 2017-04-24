563de3102a607623da413b2466f0d37e21422a81
newzko91 <tavares.gabri@gmail.com>
2017-04-24 20:30:19 -0300

    Scanner
    Scanner

---
+++ Scanner_aula/src/TesteScanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TesteScanner {
+
+	public static void main(String[] args) {
+		
+		//String str;
+		int inteiro;
+		Scanner objLeitura = new Scanner(System.in);
+		
+		//2 formas de usar o objLeitura
+			//str = objLeitura.nextLine();
+		inteiro = objLeitura.nextInt();
+		
+	}
+
+}
